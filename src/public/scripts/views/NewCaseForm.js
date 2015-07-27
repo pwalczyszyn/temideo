8367fb65e33235ef2d79272c0c1b8aa67e0fc15e
Piotr Walczyszyn <piotr.walczyszyn@gmail.com>
2015-07-27 11:39:27 +0200

    saving case code

--- src/public/scripts/views/NewCaseForm.js
+++ src/public/scripts/views/NewCaseForm.js
@@ -1,36 +1,54 @@
-import React from 'react';
+import React from 'react/addons';
 import ParseLib from 'parse';
 import ParseReact from 'parse-react';
 
 let Parse = ParseLib.Parse;
 
-console.log('Parse', Parse);
-
 var NewCaseForm = React.createClass({
-  mixins: [ParseReact.Mixin], // Enable query subscriptions
 
-  observe: function() {
-    // Subscribe to all Comment objects, ordered by creation date
-    // The results will be available at this.data.comments
+  mixins: [React.addons.LinkedStateMixin],
+
+  getInitialState: function() {
     return {
-      comments: (new Parse.Query('Comment')).ascending('createdAt')
+      fullName: undefined,
+      description: undefined,
+      email: undefined,
+      tel: undefined
     };
   },
 
+  onSubmitClick: function() {
+    var creator = ParseReact.Mutation.Create('Case', this.state);
+    creator.dispatch();
+  },
+
   render: function() {
-    // Render the text of each comment as a list item
     return (<form className="pure-form pure-form-stacked">
         <fieldset>
           <legend>Twoja sprawa</legend>
-          <textarea className="pure-input-1-2" placeholder="Opis sprawy..."></textarea>
+          <textarea className="pure-input-1-2"
+                    placeholder="Opis sprawy..."
+                    valueLink={this.linkState('description')}></textarea>
           <label htmlFor="full-name">Imię i Nazwisko</label>
-          <input id="full-name" type="text" className="pure-input-1-2" placeholder="Jan Kowalski"></input>
+          <input id="full-name"
+                 type="text"
+                 className="pure-input-1-2"
+                 placeholder="Jan Kowalski"
+                 valueLink={this.linkState('fullName')}></input>
           <label htmlFor="email">Imię i Nazwisko</label>
-          <input id="email" type="email" className="pure-input-1-2" placeholder="jan.kowalski@adres.com"></input>
+          <input id="email"
+                 type="email"
+                 className="pure-input-1-2"
+                 placeholder="jan.kowalski@adres.com"
+                 valueLink={this.linkState('email')}></input>
           <label htmlFor="tel">Numer telefonu</label>
-          <input id="tel" type="tel" className="pure-input-1-2" placeholder="555 252 525"></input>
+          <input id="tel"
+                 type="tel"
+                 className="pure-input-1-2"
+                 placeholder="555 252 525"
+                 valueLink={this.linkState('tel')}></input>
         </fieldset>
-        <button type="submit" className="pure-button pure-input-1-2 pure-button-primary">Sign in</button>
+        <button type="button" className="pure-button pure-input-1-2 pure-button-primary" onClick={this.onSubmitClick}>Sign in</button>
       </form>);
   }
 });
